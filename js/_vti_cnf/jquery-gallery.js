10acc68ca07ce6a6f917dd0089463efb48de2b1c
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2020-05-13 15:15:01 +0530

    first commit

---
+++ js/_vti_cnf/jquery-gallery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|01 May 2020 11:05:01 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|LAPTOP-VB3CPEIA\\abhishek narayan
+vti_modifiedby:SR|LAPTOP-VB3CPEIA\\abhishek narayan
+vti_timecreated:TR|31 Aug 2018 21:06:09 -0000
+vti_backlinkinfo:VX|Automatic-Gallery-Slideshow/demo.html
+vti_cacheddtm:TX|01 May 2020 11:05:01 -0000
+vti_filesize:IR|4511
+vti_nexttolasttimemodified:TR|31 Aug 2018 21:06:09 -0000
